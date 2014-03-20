8b0cc3ba9381806e798f564dc5f06b4683ad5664
wiselw <wiselw@gmail.com>
2014-03-20 17:03:08 +0800

    add locateTest and locate.html

--- SeleniumWebTest/BrowserTest.cs
+++ SeleniumWebTest/BrowserTest.cs
@@ -7,6 +7,8 @@ using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using Gallio.Framework;
 
 namespace SeleniumWebTest
 {
@@ -44,7 +46,32 @@ namespace SeleniumWebTest
             driver.Manage().Window.Maximize();//浏览器最大化，其他浏览器通用
             //可以使用如下代码指定浏览器大小
             driver.Manage().Window.Size = new System.Drawing.Size(2000,2000);
+
+
+            IWebElement element =driver.FindElement(By.Id("cname"));//单个对象定位
+
+
+            //WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+            //if (wait.Until(d => { return driver.Url.Contains("ssssss"); }))
+            //{
+            //    //开始工作
+            //}
+
             driver.Quit();
+        }
+        [Test]
+        [Name("Navigate")]
+        public void NavigateTest()
+        {
+            //
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.ctrip.com");//打开页面，记得加上http://
+            driver.Manage().Window.Maximize();//浏览器最大化，其他浏览器通
+            TestLog.WriteLine(driver.Title);//输出title
+            TestLog.WriteLine(driver.Url);//输出url
+            TestLog.WriteLine(driver.PageSource);//输出页面源码
+            driver.Quit();
+
         }
     }
 }
---
+++ SeleniumWebTest/LocateTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Gallio.Framework;
+using MbUnit.Framework;
+using MbUnit.Framework.ContractVerifiers;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System.Collections.ObjectModel;
+
+namespace SeleniumWebTest
+{
+    [TestFixture]
+    public class LocateTest
+    {
+        private IWebDriver driver;
+        [Test]
+        public void ClassNameText()
+        {
+            driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("file:///D:/Github/WebDriverForDotNet/SeleniumWebTest/locate.html");
+            driver.FindElement(By.Id("inputemail")).SendKeys("someone@163.com");
+            //driver.FindElement(By.Id("inputname")).SendKeys("some");
+            //输出 someone@163.com,不是some
+           // TestLog.WriteLine(driver.FindElement(By.ClassName("intro")).GetAttribute("value"));
+            //输出 someone@163.com
+            //TestLog.WriteLine(driver.FindElement(By.CssSelector("#inputemail")).GetAttribute("value"));
+
+            //driver.FindElement(By.PartialLinkText("度一")).Click();
+
+            //driver.FindElement(By.LinkText("百度一下")).Click();
+
+            //driver.FindElement(By.Name("email")).SendKeys("someone@163.com");
+
+            //driver.FindElement(By.TagName("input")).SendKeys("TagName");
+
+
+            //driver.FindElement(By.XPath("//input[@id='inputemail']")).SendKeys("大风");
+
+
+
+            ReadOnlyCollection<IWebElement> list = driver.FindElements(By.TagName("input"));
+            TestLog.WriteLine(list.Count);//输出2
+
+
+            string a = driver.FindElement(By.Name("email")).GetAttribute("value");
+            string b = driver.FindElement(By.Id("tbl"))
+                .FindElement(By.Name("email")).Text;
+            TestLog.WriteLine(a);//输出2
+            TestLog.WriteLine(b);//输出2
+
+            
+            
+        }
+    }
+}
